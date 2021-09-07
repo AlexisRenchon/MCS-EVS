5bde8ff7d54a896b5d38ca4ae87b43dca940a572
AlexisRenchon <a.renchon@gmail.com>
2021-09-07 15:33:39 -0500

    up

--- Code.jl
+++ Code.jl
@@ -2,12 +2,16 @@
 
 using DataFrames, CSV, StatsBase
 
-inputs = readdir(joinpath("Input", "COSORE", "datasets"), join = true)
+# get the path of all COSORE input files
+inputs = readdir(joinpath("Input", "COSORE", "datasets"), join = true);
 
+# Retrieve a short name for each dataset
 Names = []
+# in loop below,
 # 38 is the number of character in e.g., "Input/COSORE/datasets/data_d20190409_" 
 # 4 is the number of character in ".csv"
-[push!(Names, inputs[i][38:end-4]) for i = 1:length(inputs)]
+[push!(Names, inputs[i][38:end-4]) for i = 1:length(inputs)];
+Names
 
 # Some names appear more than once, e.g. MATHES, 
 # Need to numerize them e.g. MATHES1, MATHES2
@@ -29,8 +33,8 @@ end
 
 # Create a Dictionary with name => dataframe
 # e.g., Data["ZOU"] is ZOU site dataframe
-Data = Dict(Names .=> [[] for i in 1:length(Names)])
-[push!(Data[Names[i]], DataFrame(CSV.File(inputs[i]))) for i = 1:length(Names)]
+Data = Dict(Names .=> [[] for i in 1:length(Names)]);
+[push!(Data[Names[i]], DataFrame(CSV.File(inputs[i]))) for i = 1:length(Names)];
 
 # to do: reading the DataFrame, give format of datetime columns
 
