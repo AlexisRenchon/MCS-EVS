ac8b2e3393825bfd453b78578f616b0ee1203f56
AlexisRenchon <a.renchon@gmail.com>
2021-09-21 11:12:48 -0500

    updated functions

--- Functions/LoadCOSORE.jl
+++ Functions/LoadCOSORE.jl
@@ -3,14 +3,14 @@
 using DataFrames, CSV, StatsBase
 
 # get the path of all COSORE input files
-inputs = readdir(joinpath("Input", "COSORE", "datasets"), join = true);
+inputs = readdir(joinpath("..", "Input", "COSORE", "datasets"), join = true);
 
 # Retrieve a short name for each dataset
 Names = []
 # in loop below,
 # 38 is the number of character in e.g., "Input/COSORE/datasets/data_d20190409_" 
 # 4 is the number of character in ".csv"
-[push!(Names, inputs[i][38:end-4]) for i = 1:length(inputs)];
+[push!(Names, inputs[i][41:end-4]) for i = 1:length(inputs)];
 Names
 
 # Some names appear more than once, e.g. MATHES, 
