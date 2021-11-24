8284531413470a4a0f3beb0d119a97648f36eaee
AlexisRenchon <a.renchon@gmail.com>
2021-11-24 11:42:31 -0600

    added site name

--- Functions/FLUXNET_plot.jl
+++ Functions/FLUXNET_plot.jl
@@ -12,17 +12,19 @@ function FNplot(slider)
 	# slider = Slider(fig[1, 1], range = 1:5)
 	ax = Axis(fig[1, 1])	
 	site_n = slider.value
+	test = @lift(ID[$site_n])
 	data = @lift(Point2f0.(dropmissing(loadFLUXNET(ID[$site_n]), [:SW_IN_POT, :GPP_DT_CUT_50]).SW_IN_POT, 
 		               dropmissing(loadFLUXNET(ID[$site_n]), [:SW_IN_POT, :GPP_DT_CUT_50]).GPP_DT_CUT_50)) 	
 	p = plot!(ax, data)
-	return fig
+	return fig, test
 end
 
 app = App() do session::Session    
 	slider = JSServe.Slider(1:5)
-	fig = FNplot(slider)    
+	fig = FNplot(slider)[1]
+	site = FNplot(slider)[2]
 	sl = DOM.div("FLUXNET site: ", slider, slider.value)
-	return JSServe.record_states(session, DOM.div(sl, fig))
+	return JSServe.record_states(session, DOM.div(sl, site, fig))
 end
 
 
