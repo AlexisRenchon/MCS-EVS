669ceb444b2ee11107051e4afed390dd1051fd10
AlexisRenchon <a.renchon@gmail.com>
2021-11-24 11:21:43 -0600

    added MAIN_FN.jl

--- Functions/FLUXNET_plot.jl
+++ Functions/FLUXNET_plot.jl
@@ -1,9 +1,16 @@
 using WGLMakie, JSServe
 
+#=  
+;cd ..
+include(joinpath("Functions","LoadFLUXNET.jl"))
+ID = getID()[1];
+
+=#
+
 function FNplot(slider)
 	fig = Figure()
 	# slider = Slider(fig[1, 1], range = 1:5)
-	ax = Axis(fig[2, 1])	
+	ax = Axis(fig[1, 1])	
 	site_n = slider.value
 	data = @lift(Point2f0.(dropmissing(loadFLUXNET(ID[$site_n]), [:SW_IN_POT, :GPP_DT_CUT_50]).SW_IN_POT, 
 		               dropmissing(loadFLUXNET(ID[$site_n]), [:SW_IN_POT, :GPP_DT_CUT_50]).GPP_DT_CUT_50)) 	
---
+++ MAIN_FN.jl
@@ -0,0 +1,4 @@
+include(joinpath("Functions", "LoadFLUXNET.jl")) # e.g., loadFLUXNET("US-SLu")
+ID = getID()[1]; # ID for FLUXNET sites
+include(joinpath("Functions", "FLUXNET_plot.jl")) # plot light response 
+
