24e2d6266b3ae7010a6f110100e7ae1e1c70e866
AlexisRenchon <a.renchon@gmail.com>
2021-12-09 15:43:56 -0600

    PR test

--- MAIN_FN.jl
+++ MAIN_FN.jl
@@ -2,3 +2,4 @@ include(joinpath("Functions", "LoadFLUXNET.jl")) # e.g., loadFLUXNET("US-SLu")
 ID = getID()[1]; # ID for FLUXNET sites
 include(joinpath("Functions", "FLUXNET_plot.jl")) # plot light response 
 
+# PR test
